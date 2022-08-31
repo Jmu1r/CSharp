5f52bc9097bd5ea366f1f90056252821b5f21bb6
GWPC-133 <C:\Users\GWPC-133\AppData\Roaming\The Bat!>
2022-08-31 12:13:50 +0300

    hw_task4 fix

--- seminar1/hw_task4/Program.cs
+++ seminar1/hw_task4/Program.cs
@@ -14,15 +14,31 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 
-if (a > b && a > c)
+if (a == b && b == c)
+{
+Console.WriteLine("Все числа максимальные = " + a);
+}
+else if (a == b)
+{
+Console.WriteLine("Максимальные числа №1 и №2 = " + a);
+}
+else if (a == c)
+{
+Console.WriteLine("Максимальные числа №1 и №3 = " + a);
+}
+else if (b == c)
+{
+Console.WriteLine("Максимальные числа №2 и №3 = " + b);
+}
+else if (a >= b && a >= c)
 {
 Console.WriteLine("Максимальное число №1 = " + a);
 }
-else if (b > a && b > c)
+else if (b >= a && b >= c)
 {
 Console.WriteLine("Максимальное число №2 = " + b);
 }
-else if (c > a && c > b)
+else if (c >= a && c >= b)
 {
 Console.WriteLine("Максимальное число №3 = " + c);
 }
